287945c8747afe1690232e95531a488f02aa139d
Rachit2105 <rachit200521@gmail.com>
2025-09-02 15:27:29 +0530

    changes done

--- Read.py
+++ Read.py
@@ -11,13 +11,9 @@ df = pd.read_csv("AMCdata.csv" , encoding= "utf-8")
 
 df['fund_type'] = df['Scheme NAV Name'].apply(classify_fund)
 
+df = df[["ISIN Div Payout/ ISIN GrowthISIN Div Reinvestment", "Code", "fund_type"]]
+df.rename(columns={"ISIN Div Payout/ ISIN GrowthISIN Div Reinvestment" : "ISIN"} , inplace=True) 
+df.rename(columns={"fund_type" : "Fund Type"} , inplace=True) 
 
-Direct_Funds = df[df['fund_type'] == "Direct"][["ISIN Div Payout/ ISIN GrowthISIN Div Reinvestment" , "Code"]]
-Regular_Funds = df[df['fund_type'] == "Regular"][["ISIN Div Payout/ ISIN GrowthISIN Div Reinvestment" , "Code"]]
 
-Direct_Funds.columns = ["Direct ISIN" , "Direct Code"]
-Regular_Funds.columns = ["Regular ISIN" , "Regular Code"]
-
-Combined = pd.concat([Direct_Funds, Regular_Funds], axis=1)
-
-Combined.to_excel("Outfile.xlsx" , index= False)
+df.to_excel("Outfile.xlsx")
