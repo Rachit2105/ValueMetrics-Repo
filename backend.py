40439bb6028c698b1be3ec8e6d098d1932d18625
Rachit2105 <rachit200521@gmail.com>
2025-09-08 01:58:44 +0530

    backend added

--- backend.py
+++ backend.py
@@ -11,3 +11,10 @@ def get_data():
     clean_df = df.fillna("").astype(str)
     return clean_df.to_dict(orient="records")
 
+@app.get("/get-data/{name}")
+def get_data(name : str):
+    df = pd.read_excel("Outfile.xlsx")
+    
+    filtered_df = df[df['Scheme NAV Name'].str.contains(name, case=False, na=False)]
+    clean_df = filtered_df.fillna("").astype(str)
+    return clean_df.to_dict(orient="records")
