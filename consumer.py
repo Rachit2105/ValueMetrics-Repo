d1d71607e9a321911d0f410a51515088100375f8
Rachit2105 <rachit200521@gmail.com>
2025-09-17 13:15:04 +0530

    done

--- consumer.py
+++ consumer.py
@@ -1,4 +1,4 @@
-import pika
+import pika , json
 from all_required_functions import format_data
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -7,8 +7,10 @@ channel = connection.channel()
 channel.queue_declare(queue= "queue")
 
 def callback(ch, method, properties, body):
-    file_path = body.decode()  
-    format_data(file_path, "FormattedData.xlsx")    
+    data = json.loads(body)
+    file_path = data["file_path"]
+    save_file_path = data["save_file_path"]
+    format_data(file_path, save_file_path)    
 
 channel.basic_consume(queue= "queue",
                        on_message_callback= callback,
@@ -16,3 +18,4 @@ channel.basic_consume(queue= "queue",
 
 print('waiting for the message...')
 channel.start_consuming()
+print('File has been Formatted')
--- producer.py
+++ producer.py
@@ -1,13 +1,19 @@
-import pika
+import pika , json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue="queue")
 
+data = {
+        "file_path" : "AMCdata.csv",
+        "save_file_path" : "FormattedData.xlsx"
+
+}
+
 channel.basic_publish(exchange="",
                       routing_key="queue",
-                      body="AMCdata.csv")
+                      body=json.dumps(data))
 
 print('Message sent')
 connection.close()
